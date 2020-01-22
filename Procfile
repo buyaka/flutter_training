6056a52a9bc83c57c2e855fbc65badf1503e6f02
buyaka <buyaka@buyakas-MacBook-Pro.local>
2020-01-22 16:26:49 +0800

    initial import

---
+++ Procfile
@@ -0,0 +1,3 @@
+backend: cd backend && bundle install && rails s --binding 0.0.0.0 --port 8080
+frontend: cd frontend && flutter run
+#frontend: cd frontend && yarn install && yarn serve
---
+++ setup.sh
@@ -0,0 +1,23 @@
+RED="\033[1;31m"
+BLINK="\033[5m"
+NOCOLOR="\033[0m"
+
+# Dependencies
+## Ruby 2.5.0 ~
+## RoR 5.0 ~
+gem install rails --no-ri --no-rdoc
+
+## Foreman gem ~
+gem install foreman
+
+## Flutter with Dart for Mobile
+### https://flutter.dev/docs/get-started/install
+echo "${RED}Flutter install required!!! from below link${NOCOLOR}"
+echo "${BLINK}https://flutter.dev/docs/get-started/install${NOCOLOR}"
+
+
+## Flutter with Dart for Web
+### must install Chrome browser
+
+## Flutter with Dart for Desktop
+### must install Chrome browser
